0fe1b26ca90495ea6edc7223fddac22fd56ac206
SamuelMallick <sam.mallick.97@gmail.com>
2023-08-04 13:57:50 +0200

    Evaulation script with scenario and nominal mpcs

--- examples/decentralized/power_system/evaluate_power.py
+++ examples/decentralized/power_system/evaluate_power.py
@@ -4,10 +4,16 @@ from mpcrl import Agent
 import numpy as np
 import casadi as cs
 from scipy.linalg import block_diag
+from mpcrl.wrappers.envs import MonitorEpisodes
+from gymnasium.wrappers import TimeLimit
 
 from model_Hycon2 import get_model_details, get_cent_model
+from plot_power import plot_power_system_data
+from env_power import PowerSystem
 
 np.random.seed(1)
+
+SCENARIO = True
 
 (
     n,
@@ -21,9 +27,9 @@ np.random.seed(1)
     theta_lim,
     w,
     load_noise_bnd,
-) = get_model_details()  # Adj is adjacency matrix
+) = get_model_details()
 
-num_scenarios = 1   # number of scenarios for scenario MPC
+num_scenarios = 30  # number of scenarios for scenario MPC
 
 class ScenarioMpc(Mpc[cs.SX]):
     """A simple randomised scenario based MPC."""
@@ -40,6 +46,21 @@ class ScenarioMpc(Mpc[cs.SX]):
         gamma = discount_factor
         nlp = Nlp[cs.SX]()
         super().__init__(nlp, N)
+
+        # parameters
+        load = self.parameter("load", (n, 1))
+        x_o = self.parameter("x_o", (n * nx_l, 1))
+        u_o = self.parameter("u_o", (n * nu_l, 1))
+        disturb = self.parameter("disturb", (n * num_scenarios, 1))
+
+        self.fixed_parameter_dict = {
+            "load": np.array([[0], [0], [0], [0]]),
+            "x_o": np.zeros((n * nx_l, 1)),
+            "u_o": np.zeros((n * nu_l, 1)),
+            "disturb": np.random.uniform(
+                -load_noise_bnd, load_noise_bnd, (n * num_scenarios, 1)
+            ),
+        }
 
         # action is normal for scenario
         u, _ = self.action("u", n * nu_l, lb=-u_lim, ub=u_lim)
@@ -63,23 +84,6 @@ class ScenarioMpc(Mpc[cs.SX]):
         )  # n in first dim as only cnstr on theta
 
         A, B, L = get_cent_model(discrete=True)
-
-        load = self.parameter("load", (n, 1))
-        x_o = self.parameter("x_o", (n * nx_l, 1))
-        u_o = self.parameter("u_o", (n * nu_l, 1))
-        disturb = self.parameter("disturb", (n * num_scenarios, 1))
-
-        self.fixed_parameter_dict = {
-            "load": np.array([[0], [0], [0], [0]]),
-            "x_o": np.zeros((n * nx_l, 1)),
-            "u_o": np.zeros((n * nu_l, 1)),
-            "disturb": np.random.uniform(
-                # -load_noise_bnd, load_noise_bnd, (n*num_scenarios, 1)
-                -0,
-                0,
-                (n * num_scenarios, 1),
-            ),
-        }
 
         # create dynamics matrices combining all scenarios
         A_full = block_diag(*[A] * num_scenarios)
@@ -146,18 +150,143 @@ class ScenarioMpc(Mpc[cs.SX]):
         }
         self.init_solver(opts, solver="ipopt")
 
-test = ScenarioMpc()
+
+class LinearMpc(Mpc[cs.SX]):
+    """A simple linear MPC."""
+
+    horizon = prediction_length
+
+    Q_x_l = np.diag((500, 0.01, 0.01, 10))
+    Q_x = block_diag(*([Q_x_l] * n))
+    Q_u_l = 10
+    Q_u = block_diag(*([Q_u_l] * n))
+
+    def __init__(self) -> None:
+        N = self.horizon
+        gamma = discount_factor
+        nlp = Nlp[cs.SX]()
+        super().__init__(nlp, N)
+
+        load = self.parameter("load", (n, 1))
+        x_o = self.parameter("x_o", (n * nx_l, 1))
+        u_o = self.parameter("u_o", (n * nu_l, 1))
+
+        self.fixed_parameter_dict = {
+            "load": np.array([[0], [0], [0], [0]]),
+            "x_o": np.zeros((n * nx_l, 1)),
+            "u_o": np.zeros((n * nu_l, 1)),
+        }
+
+        # action is normal for scenario
+        u, _ = self.action("u", n * nu_l, lb=-u_lim, ub=u_lim)
+        x, _ = self.state("x", n * nx_l)
+        s, _, _ = self.variable(
+            "s",
+            (n, N),
+            lb=0,
+        )  # n in first dim as only cnstr on theta
+
+        # state constraints
+
+        for i in range(n):  # only a constraint on theta
+            for k in range(1, N):
+                self.constraint(
+                    f"theta_lb_{i}_{k}",
+                    -theta_lim - s[i, k],
+                    "<=",
+                    x[i * nx_l, k],
+                )
+                self.constraint(
+                    f"theta_ub_{i}_{k}",
+                    x[i * nx_l, k],
+                    "<=",
+                    theta_lim + s[i, k],
+                )
+
+        A, B, L = get_cent_model(discrete=True)
+
+        # dynamics
+        self.set_dynamics(lambda x, u: A @ x + B @ u + L @ load, n_in=2, n_out=1)
+
+        # objective
+        self.minimize(
+            +sum(
+                +(gamma**k)
+                * (
+                    (x[:, k] - x_o).T @ self.Q_x @ (x[:, k] - x_o)
+                    + (u[:, k] - u_o).T @ self.Q_u @ (u[:, k] - u_o)
+                    + w.T @ s[:, [k]]
+                )
+                for k in range(N)
+            )
+            + (gamma**N) * (x[:, N] - x_o).T @ self.Q_x @ (x[:, N] - x_o)
+        )
+
+        # solver
+        opts = {
+            "expand": True,
+            "print_time": False,
+            "bound_consistency": True,
+            "calc_lam_x": True,
+            "calc_lam_p": False,
+            # "jit": True,
+            # "jit_cleanup": True,
+            "ipopt": {
+                # "linear_solver": "ma97",
+                # "linear_system_scaling": "mc19",
+                # "nlp_scaling_method": "equilibration-based",
+                "max_iter": 1000,
+                "sb": "yes",
+                "print_level": 0,
+            },
+        }
+        self.init_solver(opts, solver="ipopt")
+
 
 class LoadedAgent(Agent):
     def on_timestep_end(self, env, episode: int, timestep: int) -> None:
         self.fixed_parameters["load"] = env.load
+        self.fixed_parameters["x_o"] = env.x_o
+        self.fixed_parameters["u_o"] = env.u_o
+        self.fixed_parameters["disturb"] = np.random.uniform(
+            -load_noise_bnd, load_noise_bnd, (n * num_scenarios, 1)
+        )
         return super().on_timestep_end(env, episode, timestep)
 
     def on_episode_start(self, env, episode: int) -> None:
         self.fixed_parameters["load"] = env.load
+        self.fixed_parameters["x_o"] = env.x_o
+        self.fixed_parameters["u_o"] = env.u_o
         return super().on_episode_start(env, episode)
 
-elif SCENARIO_BASED:
-    scen_mpc = ScenarioMpc(1)
-    agent = LoadedAgent(scen_mpc, fixed_parameters=scen_mpc.fixed_parameter_dict)
-    agent.evaluate(env=env, episodes=num_eps, seed=1)
+
+env = PowerSystem()
+num_eps = 10
+ep_len = 100
+env = MonitorEpisodes(TimeLimit(PowerSystem(), max_episode_steps=int(ep_len)))
+
+if SCENARIO:
+    mpc = ScenarioMpc()
+else:
+    mpc = LinearMpc()
+agent = LoadedAgent(mpc, fixed_parameters=mpc.fixed_parameter_dict)
+returns = agent.evaluate(env=env, episodes=num_eps, seed=1)
+
+print(f"Total returns = {sum(returns)}")
+
+# extract data
+if len(env.observations) > 0:
+    X = np.hstack([env.observations[i].squeeze().T for i in range(num_eps)]).T
+    U = np.hstack([env.actions[i].squeeze().T for i in range(num_eps)]).T
+    R = np.hstack([env.rewards[i].squeeze().T for i in range(num_eps)]).T
+else:
+    X = np.squeeze(env.ep_observations)
+    U = np.squeeze(env.ep_actions)
+    R = np.squeeze(env.ep_rewards)
+
+R_eps = [sum((R[ep_len * i : ep_len * (i + 1)])) for i in range(num_eps)]
+param_dict = {}
+time = np.arange(R.size)
+TD = []
+TD_eps = []
+plot_power_system_data(TD, R, TD_eps, R_eps, X, U)
