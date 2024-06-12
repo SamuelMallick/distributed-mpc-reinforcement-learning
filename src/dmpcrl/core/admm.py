9cccfc24d4ea63d615a0d370f3bc5cf5adf72986
SamuelMallick <sam.mallick.97@gmail.com>
2024-06-12 11:52:28 +0200

    Refactor of ADMM coordinator

--- src/dmpcrl/core/admm.py
+++ src/dmpcrl/core/admm.py
@@ -71,27 +71,24 @@ class AdmmCoordinator:
         self.nx_l = nx_l
         self.nu_l = nu_l
         self.rho = rho
+        self.N = N
 
         # create auxillary vars for ADMM procedure
-        self.y = [np.zeros((nx_l*len(self.G[i]), N + 1)) for i in range(self.n)]    # multipliers
-        # self.y_list: list[np.ndarray] = []  # dual vars
-        # self.x_temp: list[np.ndarray] = []  # intermediate numerical values for x
-        self.z = np.zeros((self.n, nx_l, N + 1))
-
-        # for i in range(self.n):
-        #     x_dim = nx_l * len(G[i])  # dimension of augmented state for agent i
-        #     # self.y_list.append(np.zeros((x_dim, N + 1)))
-        #     # self.x_temp_list.append(np.zeros((x_dim, N + 1)))
-
-        # generate slices of z for each agent, slices that will pull the relevant
-        # component of global variable out for the agents
-        # z_slices = [np.concatenate([np.arange(nx_l * j, nx_l * (j + 1)) for j in self.G[i]]) for i in range(self.n)]
-        # self.z_slices = z_slices
+        self.y = [
+            np.zeros((nx_l * len(self.G[i]), N + 1)) for i in range(self.n)
+        ]  # multipliers
+        self.augmented_x = [
+            np.zeros((nx_l * len(self.G[i]), N + 1)) for i in range(self.n)
+        ]  # augmented states
+        self.z = np.zeros((self.n, nx_l, N + 1))    # global copies of local states
 
     # TODO check if numpy arrays or DM's passed, also check return types
     def solve_admm(
-        self, state: np.ndarray, action: np.ndarray | None = None
-    ) -> tuple[list[np.ndarray], list[Solution], bool]:
+        self,
+        state: np.ndarray,
+        action: np.ndarray | None = None,
+        deterministic: bool = True,
+    ) -> tuple[list[np.ndarray], list[Solution]]:
         """Solve the mpc problem for the network of agents using ADMM. If an
         action provided, the first action is constrained to be the provided action.
 
@@ -101,75 +98,75 @@ class AdmmCoordinator:
             Global state of the network.
         action: np.ndarray | None = None
             Global action of the network. If None, the action is solved for.
+        deterministic: bool = True
+            If `True`, the cost of the MPC is perturbed according to the exploration
+            strategy to induce some exploratory behaviour. Otherwise, no perturbation is
+            performed.
 
         Returns
         -------
-        tuple[list[np.ndarray], list[Solution], bool]
-            A tuple containing the local actions, local solutions and a flag for error.
+        tuple[list[np.ndarray], list[Solution]]
+            A tuple containing the local actions, and local solutions
         """
 
-        loc_action_list = [None] * len(self.agents)
-        local_sol_list = [None] * len(self.agents)
-        x_l = np.split(state, self.n)  # split global state into local states
+        loc_action_list: list[np.ndarray] = [None] * len(self.agents)   # TODO type error on the None
+        local_sol_list: list[Solution] = [None] * len(self.agents)
+        x_l = np.split(state, self.n)  # split global state and action into local states
+        u_l = (
+            np.split(action, self.n) if action is not None else [None] * self.n
+        )  # TODO type error on the None
 
         for _ in range(self.iters):
             # x update: solve local minimisations
             for i in range(len(self.agents)):
                 # set parameters in augmented lagrangian
                 self.agents[i].fixed_parameters["y"] = self.y[i]
-                self.agents[i].fixed_parameters["z"] = self.z[self.G[i], :]  # G[i] is the agent indices relevant to agent i
+                # G[i] is the agent indices relevant to agent i. Reshape stacks them in the local augmented state
+                self.agents[i].fixed_parameters["z"] = self.z[self.G[i], :].reshape(
+                    -1, self.N + 1
+                )
 
                 if action is None:
                     loc_action_list[i], local_sol_list[i] = self.agents[i].state_value(
-                        x_l[0], deterministic
+                        x_l[i], deterministic=deterministic
                     )
                 else:
-                    # get local action from global
-                    loc_action = action[self.nu_l * i : self.nu_l * (i + 1), :]
-                    local_sol_list[i] = self.agents[i].action_value(
-                        loc_state, loc_action
-                    )
-                if not local_sol_list[i].success:  # if a local problem failed
-                    return (
-                        loc_action_list,
-                        local_sol_list,
-                        True,
-                    )  # return with error flag as true
+                    local_sol_list[i] = self.agents[i].action_value(x_l[i], u_l[i])
+                if not local_sol_list[i].success:
+                    # not raising an error on MPC failures
+                    self.agents[i].on_mpc_failure(episode=0, status=local_sol_list[i].status, raises=False) 
 
                 # construct solution to augmented state from local state and coupled states
-                idx = self.G[i].index(i) * self.nx_l
-                self.x_temp_list[i] = cs.vertcat(
-                    local_sol_list[i].vals["x_c"][:idx, :],
+                self.augmented_x[i] = cs.vertcat(
+                    local_sol_list[i].vals["x_c"][: self.nx_l * self.G[i].index(i), :],
                     local_sol_list[i].vals["x"],
-                    local_sol_list[i].vals["x_c"][idx:, :],
+                    local_sol_list[i].vals["x_c"][self.nx_l * self.G[i].index(i) :, :],
                 )
 
             # z update: an averaging of all agents' optinions on each z
-
-            for i in range(len(self.agents)):  # loop through each z
-                count = 0
-                sum = np.zeros((self.nx_l, self.z.shape[1]))
-                for j in range(
-                    len(self.agents)
-                ):  # loop through agents who have opinion on this z
-                    if i in self.G[j]:
-                        count += 1
-                        x_slice = slice(
-                            self.nx_l * self.G[j].index(i),
-                            self.nx_l * (self.G[j].index(i) + 1),
-                        )
-                        sum += self.x_temp_list[j][x_slice, :]
-                self.z[self.nx_l * i : self.nx_l * (i + 1), :] = sum / count
+            for i in range(self.n):
+                self.z[i] = np.mean(
+                    np.stack(
+                        [
+                            self.augmented_x[j][
+                                self.nx_l
+                                * self.G[j].index(i) : self.nx_l
+                                * (self.G[j].index(i) + 1),
+                                :,
+                            ]
+                            for j in self.G[i]
+                        ]
+                    ),
+                    axis=0,
+                )
 
             # y update: increment by the residual
-
-            for i in range(len(self.agents)):
-                self.y_list[i] = self.y_list[i] + self.rho * (
-                    self.x_temp_list[i] - self.z[self.z_slices[i], :]
+            for i in range(self.n):
+                self.y[i] = self.y[i] + self.rho * (
+                    self.augmented_x[i] - self.z[self.G[i], :].reshape(-1, self.N + 1)
                 )
 
         return (
             loc_action_list,
             local_sol_list,
-            False,
-        )  # return last solutions with error flag as false
+        )  # return actions and solutions from last ADMM iter
