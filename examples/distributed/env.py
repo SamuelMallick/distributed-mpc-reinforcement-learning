327fadcaeeac769d39b64a5e59e060ed76f5c6e4
mallick <sam.mallick.97@gmail.com>
2024-06-14 10:43:17 +0200

    env refactor

--- examples/distributed/env.py
+++ examples/distributed/env.py
@@ -10,11 +10,13 @@ from model import Model
 class LtiSystem(gym.Env[npt.NDArray[np.floating], npt.NDArray[np.floating]]):
     """A discrete time network of LTI systems."""
 
-    noise_bnd = np.array([[-1e-1], [0]])  # uniform noise bounds for process noise on local systems
-   
+    noise_bnd = np.array(
+        [[-1e-1], [0]]
+    )  # uniform noise bounds for process noise on local systems
+
     def __init__(self, model: Model) -> None:
         """Initializes the environment.
-        
+
         Parameters
         ----------
         model : Model
@@ -22,10 +24,12 @@ class LtiSystem(gym.Env[npt.NDArray[np.floating], npt.NDArray[np.floating]]):
         super().__init__()
         self.A, self.B = model.A, model.B
         self.n = model.n
-        self.nx = model.n*model.nx_l
+        self.nx = model.n * model.nx_l
         self.nx_l = model.nx_l
         self.x_bnd = np.tile(model.x_bnd_l, self.n)
-        self.w = np.tile([[1.2e2, 1.2e2]], (1, self.n))  # penalty weight for bound violations
+        self.w = np.tile(
+            [[1.2e2, 1.2e2]], (1, self.n)
+        )  # penalty weight for bound violations
 
     def reset(
         self,
@@ -34,14 +38,14 @@ class LtiSystem(gym.Env[npt.NDArray[np.floating], npt.NDArray[np.floating]]):
         options=None,
     ) -> tuple[npt.NDArray[np.floating], dict[str, Any]]:
         """Resets the environment.
-        
+
         Parameters
         ----------
         seed : int, optional
             The seed for the random number generator.
         options : dict[str, Any], optional
             The options for the reset.
-            
+
         Returns
         -------
         tuple[npt.NDArray[np.floating], dict[str, Any]]
@@ -53,9 +57,16 @@ class LtiSystem(gym.Env[npt.NDArray[np.floating], npt.NDArray[np.floating]]):
             self.x = np.tile([0, 0.15], self.n).reshape(self.nx, 1)
         return self.x, {}
 
-    def get_stage_cost(self, state: np.ndarray, action: np.ndarray, lb: np.ndarray, ub: np.ndarray) -> float:
+    def get_stage_cost(
+        self,
+        state: np.ndarray,
+        action: np.ndarray,
+        lb: np.ndarray,
+        ub: np.ndarray,
+        w: np.ndarray,
+    ) -> float:
         """Returns the stage cost of the system for a given state and action.
-        
+
         Parameters
         ----------
         state : np.ndarray
@@ -66,7 +77,9 @@ class LtiSystem(gym.Env[npt.NDArray[np.floating], npt.NDArray[np.floating]]):
             The lower bounds of the states.
         ub : np.ndarray
             The upper bounds of the states.
-            
+        w : np.ndarray
+            The penalty weight for bound violations.
+
         Returns
         -------
         float
@@ -74,13 +87,20 @@ class LtiSystem(gym.Env[npt.NDArray[np.floating], npt.NDArray[np.floating]]):
         return 0.5 * float(
             np.square(state).sum()
             + 0.5 * np.square(action).sum()
-            + self.w @ np.maximum(0, lb[:, np.newaxis] - state)
-            + self.w @ np.maximum(0, state - ub[:, np.newaxis])
+            + w @ np.maximum(0, lb[:, np.newaxis] - state)
+            + w @ np.maximum(0, state - ub[:, np.newaxis])
         )
 
-    def get_dist_stage_cost(self, state: np.ndarray, action: np.ndarray, lb: np.ndarray, ub: np.ndarray) -> list[float]:
+    def get_dist_stage_cost(
+        self,
+        state: np.ndarray,
+        action: np.ndarray,
+        lb: np.ndarray,
+        ub: np.ndarray,
+        w: np.ndarray,
+    ) -> list[float]:
         """Returns the distributed costs of the system for a given centralized state and action.
-        
+
         Parameters
         ----------
         state : np.ndarray
@@ -91,34 +111,52 @@ class LtiSystem(gym.Env[npt.NDArray[np.floating], npt.NDArray[np.floating]]):
             The lower bounds of the states.
         ub : np.ndarray
             The upper bounds of the states.
-            
+        w : np.ndarray
+            The penalty weight for bound violations.
+
         Returns
         -------
         list[float]
             The distributed costs."""
-        x_l, u_l, lb_l, ub_l = np.split(state, self.n), np.split(action, self.n), np.split(lb, self.n), np.split(ub, self.n)    # break into local pieces
-        return [self.get_stage_cost(x_l[i], u_l[i], lb_l[i], ub_l[i]) for i in range(self.n)]
+        x_l, u_l, lb_l, ub_l, w_l = (
+            np.split(state, self.n),
+            np.split(action, self.n),
+            np.split(lb, self.n),
+            np.split(ub, self.n),
+            np.split(w, self.n, axis=1),
+        )  # break into local pieces
+        return [
+            self.get_stage_cost(x_l[i], u_l[i], lb_l[i], ub_l[i], w_l[i])
+            for i in range(self.n)
+        ]
 
     def step(
         self, action: cs.DM
     ) -> tuple[np.ndarray, float, bool, bool, dict[str, Any]]:
         """Takes a step in the environment.
-        
+
         Parameters
         ----------
         action : cs.DM
             The action to take.
-        
+
         Returns
         -------
         tuple[np.ndarray, float, bool, bool, dict[str, Any]]
-            The new state, the reward, truncated flag, terminated flag, and an info dictionary."""
+            The new state, the reward, truncated flag, terminated flag, and an info dictionary.
+        """
         action = action.full()  # convert action from casadi DM to numpy array
         x_new = self.A @ self.x + self.B @ action
         noise = self.np_random.uniform(*self.noise_bnd).reshape(-1, 1)
-        x_new[np.arange(0, self.nx, self.nx_l)] += noise    # apply noise only to first state dimension of each agent
+        x_new[
+            np.arange(0, self.nx, self.nx_l)
+        ] += noise  # apply noise only to first state dimension of each agent
 
-        r = self.get_stage_cost(self.x, action, lb=self.x_bnd[0], ub=self.x_bnd[1])
-        r_dist = self.get_dist_stage_cost(self.x, action, lb=self.x_bnd[0], ub=self.x_bnd[1])
+        r = self.get_stage_cost(
+            self.x, action, lb=self.x_bnd[0], ub=self.x_bnd[1], w=self.w
+        )
+        r_dist = self.get_dist_stage_cost(
+            self.x, action, lb=self.x_bnd[0], ub=self.x_bnd[1], w=self.w
+        )
         self.x = x_new
         return x_new, r, False, False, {"r_dist": r_dist}
